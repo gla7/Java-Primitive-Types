adb65a77472ca5a6842f9c0340543215f3f2d043
Gabriel Leon <gabriel.leonaveleyra@tapinfluence.com>
2016-08-08 22:15:17 -0600

    second java tutorial about variables primitive types

---
+++ src/Application.java
@@ -0,0 +1,24 @@
+
+public class Application {
+
+	public static void main(String[] args) {//here we learn about primitive types of variables!
+		int myNumber;//initialize variable that is an integer
+		myNumber = 100;//assign variable
+		System.out.println(myNumber);//print variable's value
+		short myShort = 851;//for shorter numbers
+		System.out.println(myShort);
+		long myLong = 34093;//for longer numbers
+		System.out.println(myLong);
+		double myDouble = 3.14159732;//for extra precision
+		System.out.println(myDouble);
+		float myFloat = 324.4f;//point variables
+		System.out.println(myFloat);
+		char myChar = 'y';//single characters with single quotes unlike strings!
+		System.out.println(myChar);
+		boolean myBoolean = true;//booleans
+		System.out.println(myBoolean);
+		byte myByte = 127;//only 8 bits of data
+		System.out.println(myByte);
+	}
+
+}
